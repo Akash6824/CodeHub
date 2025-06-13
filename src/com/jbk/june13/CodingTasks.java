043897f596a14d8c7a1efcaf53ffd48bfa7962bc
Akash6824 <akashkesarkar@gmail.com>
2025-06-13 22:43:22 +0530

    First commit

---
+++ src/com/jbk/june13/CodingTasks.java
@@ -0,0 +1,68 @@
+package com.jbk.june13;
+
+public class CodingTasks {
+	
+	//**Dont write Hardcode..Write generic Code.... Example provided in first swapTwo method.
+
+	// Task 1 : write a code for swapping of two Numbers without using third variable.
+	
+	public static void swapTwo(int num1, int num2) {
+
+		System.out.println("Before swap: num1 = " + num1 + " and num2 = " + num2);
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		System.out.println("After swap: num1 = " + num1 + " and num2 = " + num2);
+
+	}
+
+	// Task 2 : write a code for counting the digits in a number
+	public static void digitCount(int num) {
+
+	}
+
+	// Task 3 : write a code for prime number Check
+	public static void checkPrime(int num) {
+
+	}
+
+	// Task 4 : write a code for fibonacci series upto first 10 terms.
+	public static void fibonaccieSeries(int num1, int num2) {
+
+	}
+
+	// Task 5 : write a code for Palindrome number check
+	public static void palindromeCheck(int num) {
+
+	}
+
+	// Task 6 : write a code for calculate factorial of a number
+	public static void calculateFactorial(int num) {
+
+	}
+
+	// Task 7 : write a code for to reverse the given number
+	public static void reverseOfNumber(int num) {
+
+	}
+
+	// Task 8 : write a code to check palindrome string
+	public static void stringPalindrome(String s) {
+
+	}
+
+	// Task 9 : write a code to check a Anagram Strings
+	public static void stringAnagram(String s1, String s2) {
+
+	}
+
+	// Task 10 : write a code to reverse a give String
+	public static void reverseString(String s) {
+
+	}
+
+	// Task 11 : write a code to check a perfect number
+	public static void perfectNoCheck(int num) {
+
+	}
+}
---
+++ src/com/jbk/june13/TestCodingTask.java
@@ -0,0 +1,20 @@
+package com.jbk.june13;
+
+public class TestCodingTask {
+
+	public static void main(String[] args) {
+		CodingTasks.swapTwo(20, 35);
+		CodingTasks.digitCount(654362);
+		CodingTasks.checkPrime(2487);
+		CodingTasks.fibonaccieSeries(0, 1);
+		CodingTasks.palindromeCheck(257752);
+		CodingTasks.calculateFactorial(7);
+		CodingTasks.reverseOfNumber(123564);
+		CodingTasks.stringPalindrome("racecar");
+		CodingTasks.stringAnagram("stone", "notes");
+		CodingTasks.reverseString("Hello Guys, welcome to Automation");
+		CodingTasks.perfectNoCheck(28);
+
+	}
+
+}
